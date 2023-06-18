627deed4995043d1e78de7e1d8683a2119d31b97
abdur-raqeeb ajao <ajaorqb@gmail.com>
2023-06-18 19:57:15 -0400

    chore: use base64 for production firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 import * as firebase from 'firebase-admin';
 import Config, { NODE_ENV } from '../config';
+import { generateJSONFromBase64 } from './custom';
 
 const firebaseConfig = {
     type: Config.firebase.type,
@@ -14,7 +15,7 @@ const firebaseConfig = {
     client_x509_cert_url: Config.firebase.client_x509_cert_url,
 };
 
-const serviceAccount = JSON.parse(JSON.stringify(firebaseConfig));
+const serviceAccount = NODE_ENV.DEVELOPMENT ? JSON.parse(JSON.stringify(firebaseConfig)) : generateJSONFromBase64(Config.firebaseBase64);
 
 // do not initialise app in test environment
 if (Config.nodeEnv !== NODE_ENV.TEST) {
