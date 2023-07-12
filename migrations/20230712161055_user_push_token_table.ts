d380c42f863f18bc5f78a64ae6f19b14c33deb75
MengKang <mmk070330@gmail.com>
2023-07-12 12:38:50 -0400

    fix mysql syntax error

--- migrations/20230712161055_user_push_token_table.ts
+++ migrations/20230712161055_user_push_token_table.ts
@@ -1,17 +1,17 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.raw(` CREATE TABLE IF NOT EXISTS user_push_tokens (
-        id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-        user CHAR(20) NOT NULL,
-        push_token VARCHAR(255) NOT NULL,
-        created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-        CONSTRAINT fk_user_pushToken_key
-        FOREIGN KEY (user)
-        REFERENCES users(id),
-        )
-       `);
+    await knex.raw(`
+        CREATE TABLE IF NOT EXISTS user_push_tokens (
+            id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+            user CHAR(20) NOT NULL,
+            push_token VARCHAR(255) NOT NULL,
+            created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            CONSTRAINT fk_user_pushToken_key
+                FOREIGN KEY (user)
+                REFERENCES users(id)
+        );
+    `);
 }
 
 export async function down(knex: Knex): Promise<void> {
