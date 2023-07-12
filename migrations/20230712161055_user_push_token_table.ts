19e4a3f6f4d02646f20f77b563d68cb30fe525f8
MengKang <mmk070330@gmail.com>
2023-07-12 12:29:51 -0400

    migration error

--- migrations/20230712161055_user_push_token_table.ts
+++ migrations/20230712161055_user_push_token_table.ts
@@ -2,11 +2,16 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     await knex.raw(` CREATE TABLE IF NOT EXISTS user_push_tokens (
-        id INT AUTO_INCREMENT PRIMARY KEY,
+        id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
         user CHAR(20) NOT NULL,
         push_token VARCHAR(255) NOT NULL,
         created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (user) REFERENCES users(id)`);
+
+        CONSTRAINT fk_user_pushToken_key
+        FOREIGN KEY (user)
+        REFERENCES users(id),
+
+       `);
 }
 
 export async function down(knex: Knex): Promise<void> {
